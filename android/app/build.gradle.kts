d853b13149aa9fc9c6cdb8312d207a389f43d0d5
Alejandro Giubel Hernández Arbelo <agiubel@gmail.com>
2025-03-07 22:29:02 -0500

    Update NDK version to 28.0.13004108 in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.percent_calculator"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.0.13004108"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
